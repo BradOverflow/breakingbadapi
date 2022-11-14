c6a81a1aa4c0c6a454646c8cbd709f73a1fbac09
Brady <67290724+BuddyImagine@users.noreply.github.com>
2022-11-14 10:25:59 -0500

    works

--- Breakingbadapi/Breakingbadapi/ContentView.swift
+++ Breakingbadapi/Breakingbadapi/ContentView.swift
@@ -6,71 +6,79 @@
 //
 
 
-
-
-
-//video i used
-//https://www.youtube.com/watch?v=Y-GxKhLRGyE
-
-import SwiftUI
-
+//struct for full quote contents
 struct Quote: Codable {
     var quote_id: Int
     var quote: String
     var author: String
     var series: String
 }
+//video i used
+//https://www.youtube.com/watch?v=Y-GxKhLRGyE&t=471s
 
+import SwiftUI
 
 struct ContentView: View {
+    
+    //create state variable for quotes
+    
     @State private var quotes = [Quote]()
+    
+    // fetch data function created as async
+    
+    func fetchData() async {
+        
+        //create url
+        
+        guard let url = URL(string: "https://www.breakingbadapi.com/api/quotes") else
+        {
+            print("hey man this url doesnt work ")
+            return
+        }
+        
+        //getting the jawns
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            //decode the fetched jawns from json to our quote struct jawn
+            if let decodedResponse = try? JSONDecoder().decode([Quote].self, from: data) {
+                quotes = decodedResponse
+            }
+            //catch if it doesnt work
+        } catch {
+            print("not valid")
+        }
+    }
+    
+    
+    
     var body: some View {
-        NavigationView{
-            List(quotes, id:\.quote_id) { quote in
+        NavigationView {
+            
+            //list with each jawn being titled as the quote title
+            
+            List(quotes, id: \.quote_id) { quote in
                 VStack(alignment: .leading) {
                     Text(quote.author)
-                        .font(.body)
-                        .foregroundColor(Color("skyBlue"))
+                        .font(.headline)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                     Text(quote.quote)
                         .font(.body)
-                        .foregroundColor(.secondary)
+                    
                 }
             }
             .navigationTitle("Quotes")
-            .task {
+            //The task view modifier starts the unstructured async task and binds it to the view lifecycle. SwiftUI automatically cancels ongoing tasks whenever the view disappears by propagating cooperative cancellation.
+            .task{
                 await fetchData()
             }
         }
     }
-
-   
-
-    func fetchData() async{
-            //create url
-        guard let url = URL(string: "https://www.breakingbadapi.com/api") else {
-            print("hey man THIS URL DOES NOT WORK")
-            return
-        }
-            //fetch data from url
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            //decode that data brother
-            if let decodedResponse = try? JSONDecoder().decode([Quote].self, from: data) {
-                quotes = decodedResponse
-            }
-        } catch {
-            print("bad news... this data isn't valid :(")
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
 
 
 
